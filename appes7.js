6aa3b8958addf40cd0e859b8afe8216e2d9d13aa
DavidNeiOs <davidneios25@gmail.com>
2019-05-27 04:03:17 -0400

    async await added to lib

---
+++ appes7.js
@@ -0,0 +1,31 @@
+const http = new EasyHTTP();
+
+// http
+//   .get("https://jsonplaceholder.typicode.com/users")
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// User data
+const data = {
+  name: "jhon doe",
+  username: "jhond",
+  email: "jdoe@gmail.com"
+};
+
+// create User
+// http
+//   .post("https://jsonplaceholder.typicode.com/users", data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// update User
+// http
+//   .put("https://jsonplaceholder.typicode.com/users/2", data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// delete User
+http
+  .delete("https://jsonplaceholder.typicode.com/users/2")
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
---
+++ easyhttp3.js
@@ -0,0 +1,58 @@
+/**
+ * Easy http library
+ *
+ * @version 2.0.0
+ * @author Brad Traversy
+ * @license MIT
+ **/
+
+class EasyHTTP {
+  // make an http get request
+  async get(url) {
+    const response = await fetch(url);
+
+    const resData = await response.json();
+    return resData;
+  }
+
+  // Make an HTTP post request
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    const resData = await response.json();
+    return resData;
+  }
+
+  // Make a put request
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    const resData = await response.json();
+    return resData;
+  }
+
+  // make an http delete Request
+  async delete(url, data) {
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json"
+      }
+    });
+    if (response.status === 200) {
+      return "Resource deleted";
+    } else {
+      return new Error("something went wrong");
+    }
+  }
+}
